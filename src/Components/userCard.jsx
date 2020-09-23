14ac44439c3ffc99f73422e6639d1603112977c4
alexiliev <iliev.alex@outlook.com>
2020-09-23 13:20:26 -0500

    adding Tags to each Card

--- src/Components/userCard.jsx
+++ src/Components/userCard.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { Card } from 'antd'
+import { Card, Tag } from 'antd'
 import 'antd/dist/antd.css';
 const { Meta } = Card;
 
@@ -12,12 +12,21 @@ export default class userCard extends Component {
             style={{ width: 240 }}
             cover={
             <img
-                alt='example'
+                alt='trumpet'
                 src='https://images.unsplash.com/photo-1573871669414-010dbf73ca84?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=668&q=80'
             />
             }
         >
-          <Meta title='John' description="Let's jam at my place this weekend!" />
+        
+            <Meta 
+                title='John' 
+                description="Let's jam at my place this weekend!"
+            />
+                <Tag>Jam</Tag>
+                <Tag>Lessons</Tag>
+                <Tag>Join a Band</Tag>
+                <Tag>Perform Live</Tag>
+                <Tag>Record Song</Tag>
         </Card>
         ,
       </div>
